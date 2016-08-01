ce71a47f8c6c334b5902fae7a7bdab3e168eb768
James <jaimersonaraujo@gmail.com>
2016-08-01 10:24:50 -0300

    Create users

---
+++ db/migrate/20160801132305_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false, unique: true
+
+      t.timestamps null: false
+    end
+  end
+end
